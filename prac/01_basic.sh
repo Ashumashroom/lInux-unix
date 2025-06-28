568211079e92ee8914f4cdf142d78518d63fc64c
Ashish Ranjan <ashsihranjan6288@gmail.com>
2025-06-29 02:51:37 +0530

    Meaningful message about what you changed

---
+++ prac/01_basic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# -----------------------------------------------
+# Script Name: hello.sh
+# Description: Prints a friendly message to the user.
+# Author: YourName (optional)
+# Usage: ./hello.sh
+# -----------------------------------------------
+
+# Print a welcome message
+echo "HEY BUDDY!"
+
+# This is a single-line comment
+
+: <<'MULTILINE_COMMENT'
+This is a
+multi-line comment.
+You can use this block for notes, TODOs, or documentation
+without affecting script execution.
+MULTILINE_COMMENT
